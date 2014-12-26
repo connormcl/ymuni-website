6ccd5f5abd8117c9ec41c96ed99818c4271a5649
Connor McLaughlin <connormcl14@gmail.com>
2014-12-26 02:09:17 -0500

    added vision, curriculum, faq, dates and deadlines, financial aid and apply pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,25 @@
 class WelcomeController < ApplicationController
 	def about
 	end
+
+	def vision
+	end
+
+	def curriculum
+	end
+
+	def team
+	end
+
+	def dates
+	end
+
+	def financial
+	end
+
+	def faq
+	end
+
+	def apply
+	end
 end
